6fdb51da9685a739b9017a048d092ef7619e3171
Jongmin <wjm7721@naver.com>
2022-10-14 03:27:48 +0900

    first commit

---
+++ 킹, 퀸, 룩, 비숍, 나이트, 폰.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int a, b, c, d, e, f;
+    scanf("%d %d %d %d %d %d", &a, &b, &c, &d, &e, &f);
+
+    a = (a == 1) ? 0 : 1 - a;
+    b = (b == 1) ? 0 : 1 - b;
+    c = (c == 2) ? 0 : 2 - c;
+    d = (d == 2) ? 0 : 2 - d;
+    e = (e == 2) ? 0 : 2 - e;
+    f = (f == 8) ? 0 : 8 - f;
+
+    printf("%d %d %d %d %d %d", a, b, c, d, e, f);
+
+    return 0;
+}
