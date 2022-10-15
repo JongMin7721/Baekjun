6c12ab3260449ba4254168f25619420ad39cae99
Jongmin <wjm7721@naver.com>
2022-10-15 10:33:51 +0900

    구구단

---
+++ 단계별로 풀어보기/3단계/구구단.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int a;
+    scanf("%d", &a);
+
+    printf("%d * 1 = %d\n", a, a);
+    printf("%d * 2 = %d\n", a, a * 2);
+    printf("%d * 3 = %d\n", a, a * 3);
+    printf("%d * 4 = %d\n", a, a * 4);
+    printf("%d * 5 = %d\n", a, a * 5);
+    printf("%d * 6 = %d\n", a, a * 6);
+    printf("%d * 7 = %d\n", a, a * 7);
+    printf("%d * 8 = %d\n", a, a * 8);
+    printf("%d * 9 = %d\n", a, a * 9);
+
+    return 0;
+}
