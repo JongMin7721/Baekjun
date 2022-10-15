ffd39ffed1836fcaf961fb8a5b028694e85efb6e
Jongmin <wjm7721@naver.com>
2022-10-15 18:47:06 +0900

    반복문 정정

--- 단계별로 풀어보기/3단계/구구단.c
+++ 단계별로 풀어보기/3단계/구구단.c
@@ -2,18 +2,13 @@
 
 int main(void)
 {
-    int a;
+    int a, i;
     scanf("%d", &a);
 
-    printf("%d * 1 = %d\n", a, a);
-    printf("%d * 2 = %d\n", a, a * 2);
-    printf("%d * 3 = %d\n", a, a * 3);
-    printf("%d * 4 = %d\n", a, a * 4);
-    printf("%d * 5 = %d\n", a, a * 5);
-    printf("%d * 6 = %d\n", a, a * 6);
-    printf("%d * 7 = %d\n", a, a * 7);
-    printf("%d * 8 = %d\n", a, a * 8);
-    printf("%d * 9 = %d\n", a, a * 9);
+    for (i = 1; i < 10; i++)
+    {
+        printf("%d * %d = %d\n", a, i, a * i);
+    }
 
     return 0;
 }
