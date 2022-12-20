b617011ba27e058e045379c1919ca47be1fd585f
JongMin7721 <wjm7721@naver.com>
2022-12-20 17:47:21 +0900

    백준

--- 랭크/1.브론즈/1809-Moo.c
+++ 랭크/1.브론즈/1809-Moo.c
@@ -4,3 +4,6 @@ int main(void) // 언어제한 Golfscript
 {
     printf("(___)\n(o o)____/\n @@      \\\n  \\ ____,/\n  //   //\n ^^   ^^");
 }
+
+// Golfscript
+// printf"(___)\n(o o)____/\n @@      \\\n  \\ ____,/\n  //   //\n ^^   ^^"
