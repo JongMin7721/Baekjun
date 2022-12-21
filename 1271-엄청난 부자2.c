769c46e2632a14d5ecd5359d780dd715527c7bb3
JongMin7721 <wjm7721@naver.com>
2022-12-21 23:30:10 +0900

    백준

--- 1271-엄청난 부자2.c
+++ 1271-엄청난 부자2.c
@@ -1,11 +1,20 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-int main(void) // 문자열 이용하여 풀기
+int main(void) // 문자열 이용하여 풀기 // 다음에 풀기
 {
-    int n, m;
-    scanf("%d %d", &n, &m);
+    char bignum[1001], smallnum[1001], res[1001];
+    scanf("%s %s", &bignum, &smallnum);
+    int l = strlen(smallnum), ll = strlen(bignum), lnl = ll - l;
 
-    printf("%d\n%d", n / m, n % m);
+    for (int i = 0; i < lnl; i++)
+    {
+        /* code */
+    }
+
+    printf("%d %s\n", atoi(bignum) - atoi(smallnum), smallnum);
+    printf("%s", bignum);
 
     return 0;
 }
