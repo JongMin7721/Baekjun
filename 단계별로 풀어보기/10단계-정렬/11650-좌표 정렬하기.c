d4a35a365b93c1351eef544224fc724a021ace77
Jongmin <wjm7721@naver.com>
2022-12-28 00:42:54 +0900

    백준

---
+++ 단계별로 풀어보기/10단계-정렬/11650-좌표 정렬하기.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct
+{
+    int x, y;
+} coordinate;
+
+int compare(const void *first, const void *second);
+
+int main(void)
+{
+    coordinate coord[100000];
+    int n;
+    scanf("%d", &n);
+
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d %d", &coord[i].x, &coord[i].y);
+    }
+
+    qsort((coordinate *)coord, n, sizeof(coordinate), compare);
+
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d %d\n", coord[i].x, coord[i].y);
+    }
+}
+
+int compare(const void *first, const void *second)
+{
+    if (((coordinate *)first)->x > ((coordinate *)second)->x)
+    {
+        return 1;
+    }
+    else if (((coordinate *)first)->x < ((coordinate *)second)->x)
+    {
+        return -1;
+    }
+    else
+    {
+        if (((coordinate *)first)->y > ((coordinate *)second)->y)
+        {
+            return 1;
+        }
+        else if (((coordinate *)first)->y < ((coordinate *)second)->y)
+        {
+            return -1;
+        }
+        else
+        {
+            return 0;
+        }
+    }
+}
