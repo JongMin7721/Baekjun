7f1e4330857b66f90c70c75359d5d106f25cc49e
Jongmin <wjm7721@naver.com>
2023-01-05 00:13:27 +0900

    백준

---
+++ 9316-Hello Judge.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int n;
+
+    scanf("%d", &n);
+
+    for (int i = 0; i < n; i++)
+    {
+        printf("Hello World, Judge %d!\n", i + 1);
+    }
+
+    return 0;
+}
