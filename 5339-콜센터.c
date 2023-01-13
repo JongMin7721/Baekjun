649f21b9642fb1699f725c903e1ae5da826bc347
Jongmin <wjm7721@naver.com>
2023-01-14 00:38:55 +0900

    백준

---
+++ 5339-콜센터.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(void)
+{
+    printf("     /~\\\n    ( oo|\n    _\\=/_\n   /  _  \\\n  //|/.\\|\\\\\n ||  \\ /  ||\n============\n|          |\n|          |\n|          |\n");
+
+    return 0;
+}
