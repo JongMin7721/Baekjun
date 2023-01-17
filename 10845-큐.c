7011bff7b7fa4ef5f41c6d389ae73d5c943c1a6c
wjm7721@gmail.com <wjm7721@naver.com>
2023-01-17 13:12:00 +0900

    백준

---
+++ 10845-큐.c
@@ -0,0 +1,103 @@
+#include <stdio.h>
+#include <string.h>
+#define MAX_SIZE_QUEUE 10000
+
+void push(int num);
+int pop();
+int size();
+int empty();
+int front();
+int back();
+
+int arr[MAX_SIZE_QUEUE];
+int f = -1;
+int r = -1;
+
+int main(void)
+{
+    int n, num;
+    char m[8];
+
+    scanf("%d", &n);
+
+    for (int i = 0; i < n; i++)
+    {
+        scanf(" %s", m);
+
+        if (!strcmp(m, "push"))
+        {
+            scanf(" %d", &num);
+            push(num);
+        }
+        else if (!strcmp(m, "pop"))
+        {
+            printf("%d\n", pop());
+        }
+        else if (!strcmp(m, "size"))
+        {
+            printf("%d\n", size());
+        }
+        else if (!strcmp(m, "empty"))
+        {
+            printf("%d\n", empty());
+        }
+        else if (!strcmp(m, "front"))
+        {
+            printf("%d\n", front());
+        }
+        else if (!strcmp(m, "back"))
+        {
+            printf("%d\n", back());
+        }
+    }
+
+    return 0;
+}
+
+void push(int num)
+{
+    r = ++r % MAX_SIZE_QUEUE;
+    arr[r] = num;
+    return;
+}
+
+int pop()
+{
+    if (empty())
+    {
+        return -1;
+    }
+
+    f = ++f % MAX_SIZE_QUEUE;
+    return arr[f];
+}
+
+int size()
+{
+    return r - f;
+}
+
+int empty()
+{
+    return r - f == 0;
+}
+
+int front()
+{
+    if (empty())
+    {
+        return -1;
+    }
+
+    return arr[f + 1];
+}
+
+int back()
+{
+    if (empty())
+    {
+        return -1;
+    }
+
+    return arr[r];
+}
