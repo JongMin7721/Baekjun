ac565d496556779afb093ae9c9a7a53e37c10c12
JongMin7721 <wjm7721@naver.com>
2023-01-23 17:32:18 +0900

    .

--- sfd;jf.c
+++ sfd;jf.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
 
+void print_star(int n);
+void p13star(int n);
+void p2star(int n);
+
 int main(void)
 {
     int n, lg = 0;
@@ -10,16 +14,46 @@ int main(void)
     {
         lg++;
     }
+
+    print_star(n);
 }
 
-void print_star()
+void print_star(int n)
 {
+    for (int i = 0; i < n; i++)
+    {
+        if (n > 9)
+        {
+            print_star(n / 3);
+        }
+        p2star(n);
+        printf("\n");
+    }
 }
 
-void p13star()
+void p13star(int n)
 {
+    for (int i = 0; i < n * 3; i++)
+    {
+        printf("*");
+    }
+    printf("\n");
 }
 
-void p2star()
+void p2star(int n)
 {
+    int i = 0;
+    for (; i < n; i++)
+    {
+        printf("*");
+    }
+    for (; i < n * 2; i++)
+    {
+        printf(" ");
+    }
+    for (; i < n * 3; i++)
+    {
+        printf("*");
+    }
+    printf("\n");
 }
