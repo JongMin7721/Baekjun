4a9e16e9a9f6af40b2e3a5acbdd8790479a13db5
wjm7721@gmail.com <wjm7721@naver.com>
2023-01-31 16:59:06 +0900

    .

---
+++ 1952-달팽이2.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void) // 알고리즘 다시 구상
+{
+    int i = 0, j = 0, a = 0, b = 0, n, m, turn = 0;
+    int nm[100][100] = {0};
+    scanf("%d %d", &n, &m);
+
+    while (1)
+    {
+        }
+
+    printf("%d\n", turn);
+}
