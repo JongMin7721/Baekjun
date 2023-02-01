8a23cd4a259512b7211260b6fb20e71b20fd15bb
wjm7721@gmail.com <wjm7721@naver.com>
2023-02-01 15:19:43 +0900

    .

--- 1952-달팽이2.c
+++ 1952-달팽이2.c
@@ -1,14 +1,39 @@
 #include <stdio.h>
 
-int main(void) // 알고리즘 다시 구상
+int main(void)
 {
-    int i = 0, j = 0, a = 0, b = 0, n, m, turn = 0;
-    int nm[100][100] = {0};
+    int i = 0, j = 0, n, m, turn = 0;
     scanf("%d %d", &n, &m);
 
     while (1)
     {
+        switch (turn % 4)
+        {
+        case 0:
+            i++;
+            break;
+        case 1:
+            m--;
+            break;
+        case 2:
+            n--;
+            break;
+        case 3:
+            j++;
+            break;
+        default:
+            break;
+        }
+
+        if (i == n || j == m)
+        {
+            break;
+        }
+
+        turn++;
     }
 
     printf("%d\n", turn);
+
+    return 0;
 }
--- 연습장.cpp
+++ 연습장.cpp
@@ -1,54 +1,99 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <stdlib.h>
+#include <malloc.h>
 #include <string.h>
+#include <math.h>
 
-#define SIZE 81
-
-int main(void)
+int compare(const void *a, const void *b)
 {
-	int c = 0; // case
+	int num1 = *(int *)a;
+	int num2 = *(int *)b;
 
-	int sum_O[SIZE];	 // X면 0,O면 증가시킨값 더하기를 각 배열요소에 집어 넣기 위한 배열
-	int sum_score[SIZE]; // 각 케이스별 배열의 합
+	if (num1 < num2)
+		return -1;
 
-	char yes_no[SIZE] = " ";
-
-	scanf("%d", &c);
-
-	for (int i = 0; i < c; i++)
-	{
-		// 각 케이스 별(예를들어 i=0일때) 배열에 o,x값 입력(scanf는 띄워쓰기를 하면 버퍼(%s)에 띄워쓰기 전까지의 값을 넣어버림)
-		scanf("%s", yes_no);
-		// sum_O의 배열요소는 1부터 시작하므로 [0]은 0으로 초기화 시켜준다.
-		sum_O[0] = {0};
-		// o,x를 구분하기 위해 배열 전체 조사를 위한 반복문(배열요소1부터 시작->전의 직전 배열 요소의 값과 현재값과의 합을 위함)
-
-		int len = strlen(yes_no); // 문자열의 길이를 구하기위하여(시간초과 오류는 아님)
-
-		for (int j = 1; j < len ; j++)
-		{
-
-			if (yes_no[j] == 'O')
-			{
-				sum_O[j] = sum_O[j - 1] + 1;
-			}
-			else if (yes_no[j] == 'X')
-			{
-				sum_O[j] = 0;
-			}
-		}
-		// 각 케이스별 총합 구하기
-		for (int j = 1; j < len + 1; j++)
-		{
-			sum_score[i] += sum_O[j];
-		}
-		// 다음 케이스를 위하여 배열 초기화(문자열은 '\0'로 초기화 해준다)
-		yes_no[0] = '\0';
-	}
-
-	for (int i = 0; i < c; i++)
-		// 각 케이스 별 구한 결과의 합 출력하기
-		printf("%d\n", sum_score[i]);
+	if (num1 > num2)
+		return 1;
 
 	return 0;
+}
+
+void sansul(int list[], int n)
+{
+	double sum = 0;
+	for (int i = 0; i < n; i++)
+	{
+		sum += (list[i]);
+	}
+	printf("%d\n", (int)round(sum / n));
+}
+
+void middle(int list[], int n)
+{
+	if (n == 1)
+		printf("%d\n", list[0]);
+	else
+		printf("%d\n", list[n / 2]);
+}
+
+void max_value(int list[], int n)
+{
+	int a[8001] = {
+		0,
+	};
+	int max = 0;
+	int idx;
+	int count = 0;
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		idx = list[i] + 4000;
+		a[idx] += 1;
+
+		if (a[idx] > max)
+			max = a[idx];
+	}
+	for (i = 0, idx = 0; i < 8001; i++)
+	{
+		if (a[i] == 0)
+			continue;
+		if (a[i] == max)
+		{
+			if (count == 0)
+			{
+				idx = i;
+				count++;
+			}
+			else if (count == 1)
+			{
+				idx = i;
+				break;
+			}
+		}
+	}
+	printf("%d\n", idx - 4000);
+}
+
+void length(int list[], int n)
+{
+	printf("%d\n", list[n - 1] - list[0]);
+}
+
+int main()
+{
+	int n;
+	scanf("%d", &n);
+	int *a = (int *)malloc(sizeof(int) * n);
+
+	for (int i = 0; i < n; i++)
+	{
+		scanf("%d", &a[i]);
+	}
+	qsort(a, n, sizeof(a[0]), compare);
+	sansul(a, n);
+	middle(a, n);
+	max_value(a, n);
+	length(a, n);
 }
