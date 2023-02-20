4fe965b73af78127ac77dea808ff63fbeeaf4c69
wjm7721@gmail.com <wjm7721@naver.com>
2023-02-20 10:06:23 +0900

    .

---
+++ 1sda.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int arr[2187][2187], N;
+
+    scanf("%d", &N);
+
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = 0; j < N; j++)
+        {
+        }
+    }
+}
--- 연습장.cpp
+++ 연습장.cpp
@@ -1,29 +1,68 @@
 #include <iostream>
+#include <algorithm>
 
 using namespace std;
 
+int compare(const void *a, const void *b)
+{
+	int *x = (int *)a;
+	int *y = (int *)b;
+
+	if (*x > *y)
+		return 1;
+	else if (*x < *y)
+		return -1;
+	else
+		return 0;
+}
+
+int myFind(int *arr, int left, int right, int n)
+{
+	int mid = (left + right) / 2;
+
+	while (left < mid)
+	{
+		mid = (left + right) / 2;
+
+		if (*(arr + mid) == n)
+		{
+			return 1;
+		}
+		else if (*(arr + mid) > n)
+		{
+			right = mid;
+		}
+		else
+		{
+			left = mid + 1;
+		}
+	}
+
+	return 0;
+}
+
 int main()
 {
-	int a, b, v;
+	int size, n;
+	cin >> size;
 
-	cin >> a >> b >> v;
+	int *arr = new int[size];
 
-	int h = v - a;	 // 전체거리 - A
-	int win = a - b; // 하루동안 최종 이동 거리
-	int days = 0;
-
-	if (h < win)
-	{ //(전체거리-A) < (낮~밤 이동 거리)
-		days = 2;
-	}
-	else if (h % win == 0)
-	{ // 낮에 한번만 더 올라가면 되는 경우
-		days = h / win + 1;
-	}
-	else if (h % win != 0)
-	{ // 이틀 더 올라가야 하는 경우
-		days = h / win + 2;
+	for (int i = 0; i < size; i++)
+	{
+		cin >> n;
+		*(arr + i) = n;
 	}
 
-	cout << days << "\n";
+	qsort(arr, size, sizeof(int), compare);
+
+	int nCheck;
+	cin >> nCheck;
+	while (nCheck-- > 0)
+	{
+		cin >> n;
+		cout << myFind(arr, 0, size, n) << ' ';
+	}
+
+	return 0;
 }
