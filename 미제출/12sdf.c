7ebcc0f6f98fb105b2b6c153389e485d82bc42b7
wjm7721@gmail.com <wjm7721@naver.com>
2023-02-28 16:59:20 +0900

    .

---
+++ 미제출/12sdf.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+char dic[100000][21];
+
+int find_dic(char *ans, int N)
+{
+    for (int i = 0; i < N; i++)
+    {
+        if (!strcmp(ans, dic[i]))
+        {
+            return i;
+        }
+    }
+}
+
+int main(void)
+{
+    int N, M, num;
+    char ans[21];
+
+    scanf("%d %d", &N, &M);
+
+    for (int i = 0; i < N; i++)
+    {
+        scanf("%s", dic[i]);
+    }
+
+    for (int i = 0; i < M; i++)
+    {
+        scanf("%s", ans);
+
+        if (ans[0] >= '0' && ans[0] <= '9')
+        {
+            num = atoi(ans);
+            printf("%s\n", dic[num - 1]);
+        }
+        else
+        {
+            printf("%d\n", find_dic(ans, N) + 1);
+        }
+    }
+
+    return 0;
+}
