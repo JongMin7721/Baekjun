64d632d66fe8c3a38d94f634f0f1514167097dd1
Jongmin <wjm7721@naver.com>
2023-04-23 02:55:36 +0900

    .

---
+++ 랭크/1.브론즈/15680-연세대학교.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int n;
+
+    scanf("%d", &n);
+
+    if (n)
+    {
+        printf("Leading the Way to the Future\n");
+    }
+    else
+    {
+        printf("YONSEI\n");
+    }
+
+    return 0;
+}
