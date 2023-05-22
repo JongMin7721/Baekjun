dbc6f8ca56b0ffc0f70f0507013deb673c655b70
Jongmin <wjm7721@naver.com>
2023-05-23 00:28:50 +0900

    .

---
+++ 랭크/1.브론즈/10953-A+B - 6.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int a, b, n;
+
+    scanf("%d", &n);
+
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d,%d", &a, &b);
+
+        printf("%d\n", a + b);
+    }
+
+    return 0;
+}
