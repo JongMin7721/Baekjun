33a7313957c4e89e96314f62b601c8d0cf2ea7ae
Jongmin <wjm7721@naver.com>
2023-05-25 05:55:03 +0900

    .

---
+++ 랭크/1.브론즈/5300-Fill the Rowboats!.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int n;
+
+    scanf("%d", &n);
+
+    for (int i = 1; i <= n; i++)
+    {
+        printf("%d ", i);
+        if (i % 6 == 0)
+        {
+            printf("Go! ");
+        }
+    }
+
+    if (n % 6 != 0)
+    {
+        printf("Go!");
+    }
+
+    printf("\n");
+
+    return 0;
+}
